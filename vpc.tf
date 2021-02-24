cf1ae17a03c9cdd70dc2a369a3da3c01dc700f1c
Victor Jimenez <betabandido@gmail.com>
2021-02-24 15:50:57 +0100

    Fix reference to VPC

--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,5 @@
-data "aws_vpc" "eks_vpc" {
-  tags = {
-    Name = data.aws_eks_cluster.cluster.vpc_config[0].vpc_id
-  }
-}
-
 data "aws_subnet_ids" "private" {
-  vpc_id = data.aws_vpc.eks_vpc.id
+  vpc_id = data.aws_eks_cluster.cluster.vpc_config[0].vpc_id
 
   tags = {
     Type = "Private"
