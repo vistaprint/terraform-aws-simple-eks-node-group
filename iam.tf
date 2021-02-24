0792275c9ce953655b65d360da6475d723113236
Victor Jimenez <betabandido@gmail.com>
2021-02-24 17:08:23 +0100

    Missing bits

--- iam.tf
+++
@@ -1,3 +0,0 @@
-data "aws_iam_role" "worker_role" {
-  name = "${var.cluster_name}-eks-worker-role"
-}
--- vpc.tf
+++
@@ -1,7 +0,0 @@
-data "aws_subnet_ids" "private" {
-  vpc_id = data.aws_eks_cluster.cluster.vpc_config[0].vpc_id
-
-  tags = {
-    Type = "Private"
-  }
-}
