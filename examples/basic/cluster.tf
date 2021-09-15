ab8fce95e07201bc4bfe92457680101d6c44a8d4
xorxsan <36991286+xorxsan@users.noreply.github.com>
2021-09-15 12:24:16 +0200

    Add support for http tokens in metadata endpoint (#3)
    * Add support for http tokens in metadata endpoint

---
+++ examples/basic/cluster.tf
@@ -0,0 +1,38 @@
+module "cluster" {
+  source  = "vistaprint/simple-eks/aws"
+  version = "0.3.3"
+
+  cluster_name    = "simple-eks-integration-test-for-eks-node-group"
+  cluster_version = "1.20"
+  vpc_name        = var.vpc_name
+  log_group_name  = "a-test-log-group-name"
+
+  region  = var.aws_region
+  profile = var.aws_profile
+}
+
+module "on_demand_node_group" {
+  source = "../.."
+
+  cluster_name       = "simple-eks-integration-test-for-eks-node-group"
+  node_group_name    = "on-demand"
+  node_group_version = "1.20"
+
+  instance_types = ["t3a.small"]
+
+  scaling_config = {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  worker_role_arn = module.cluster.worker_role_arn
+  subnet_ids      = module.cluster.private_subnet_ids
+
+  use_calico_cni = false
+
+  region  = var.aws_region
+  profile = var.aws_profile
+
+  depends_on = [module.cluster]
+}
